b5d4e4fa0b8dcf3373c48f1e2190b0567fc8155c
Rei You <rei.you@ibm.com>
2022-09-18 15:24:21 +0900

    create project

---
+++ lottery/lottery/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  lottery
+//
+//  Created by your3i on 2022/09/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lottery/lottery/lotteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  lotteryApp.swift
+//  lottery
+//
+//  Created by your3i on 2022/09/18.
+//
+
+import SwiftUI
+
+@main
+struct lotteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
